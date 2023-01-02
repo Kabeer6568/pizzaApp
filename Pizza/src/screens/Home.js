b5ccfbed3ca7c08ed111fb816e0152d5381ba227
Kabeer Ali Alvi <kabeeralvi6568@gmail.com>
2023-01-02 21:37:21 +0500

    Home screen almost complete

--- Pizza/src/screens/Home.js
+++ Pizza/src/screens/Home.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity, Button} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+// import Pizza1 from '../assets/img/Pizza_1'
 
 const Home = () => {
   return (
-    <View>
-      <LinearGradient
-        colors={['#F5313F', '#FFA360']}
-        style={[styles.linearGradient, styles.mainBanner]}
-        start={{x: 0, y: 0.9}}
-        end={{x: 1, y: 0.2}}>
+    <>
+      <View>
+        <LinearGradient
+          colors={['#F5313F', '#FFA360']}
+          style={[styles.linearGradient, styles.mainBanner]}
+          start={{x: 0, y: 0.9}}
+          end={{x: 1, y: 0.2}}>
           <View style={styles.mainBox1}>
-        <Text style={styles.mainHeading}>Create Your Pizza</Text>
-        <Text style={styles.subHeading}>SIZE, CRUST, TOPPINGS</Text>
+            <Text style={styles.mainHeading}>Create Your Pizza</Text>
+            <Text style={styles.subHeading}>SIZE, CRUST, TOPPINGS</Text>
           </View>
           <View style={styles.mainBox2}>
-        <Text style={styles.mainPricing}>$10</Text>
+            <Text style={styles.mainPricing}>$10</Text>
           </View>
-      </LinearGradient>
-    </View>
+        </LinearGradient>
+      </View>
+
+      <View style={{borderWidth: 0,marginLeft: 30,marginRight: 30,backgroundColor: 'rgba(255, 255, 255, 0.6)',marginTop: -120,  borderRadius: 200,paddingTop: 20,paddingBottom: 20}}>
+        <View style={{borderWidth: 0,marginLeft: 30,marginRight: 30,backgroundColor: '#fcf8f6',  borderRadius: 200,paddingTop: 20,paddingBottom: 20}}>
+          <View style={{alignItems: 'center', justifyContent: 'center'}}>
+            <Image
+              style={styles.MainPizza}
+              source={require('../assets/img/Pizza_1.png')}
+            />
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.pizzaSizesBTNContainer}>
+        <View style={{flex: 0.3}}></View>
+        <TouchableOpacity 
+        style={styles.pizzaSizesBTN}
+        >
+          <Text>
+            12*
+          </Text>
+        </TouchableOpacity>
+        <View style={{flex: 0.3}}></View>
+      </View>
+        
+
+      <View style={styles.pizzaSize}>
+      <Text style={styles.pizzaSizeHeading}>
+          Choose your size
+        </Text>
+      </View>
+    </>
   );
 };
 
@@ -31,14 +64,13 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
     borderRadius: 5,
-    height: 200,
+    height: 250,
     width: '100%',
   },
-  mainBox1:{
+  mainBox1: {
     flex: 0.7,
   },
   mainHeading: {
-    
     marginTop: 20,
     marginLeft: 26,
     color: 'white',
@@ -50,7 +82,6 @@ const styles = StyleSheet.create({
     letterSpacing: -0.3,
   },
   subHeading: {
-    
     marginTop: 0,
     marginLeft: 26,
     color: '#EAEAEA',
@@ -61,10 +92,10 @@ const styles = StyleSheet.create({
     lineHeight: 29,
     letterSpacing: -0.3,
   },
-  mainBox2:{
+  mainBox2: {
     flex: 0.3,
   },
-  mainPricing:{
+  mainPricing: {
     marginTop: 20,
     marginLeft: 26,
     color: 'white',
@@ -74,6 +105,46 @@ const styles = StyleSheet.create({
     fontSize: 26,
     lineHeight: 29,
     letterSpacing: -0.3,
+  },
+  MainPizza: {
+    width: 200,
+    height: 200,
+  },
+  pizzaSizesBTNContainer:{
+    // flex: 0.5,
+    flexDirection: 'row',
+    marginTop: 10,
+    padding: 10,
+  },
+  pizzaSizesBTN:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 0.4,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderRadius: 20,
+    backgroundColor: '#dddddd'
+  },
+  pizzaSize:{
+    backgroundColor: '#fff',
+    height: 120,
+    margin: 20,
+    borderRadius: 30,
+    shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 2,
+},
+shadowOpacity: 0.25,
+shadowRadius: 3.84,
+elevation: 5,
+  },
+  pizzaSizeHeading:{
+    fontSize: 20,
+    textAlign: 'center',
+    paddingTop: 10,
   }
 });
 
