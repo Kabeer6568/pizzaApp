9952115529224c292778b55c4ac350d570c87038
Kabeer Ali Alvi <kabeeralvi6568@gmail.com>
2023-01-03 22:05:02 +0500

    Home Screen Done

--- Pizza/App.js
+++ Pizza/App.js
@@ -2,14 +2,17 @@
 // In App.js in a new project
 // import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Tabs from './src/components/tabNavigation'
 import Home from './src/screens/Home'
 import About from './src/screens/About'
 
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 // function HomeScreen() {
 //   return (
@@ -23,6 +26,7 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
+    <>
     <NavigationContainer>
       <Drawer.Navigator>
       <Drawer.Screen name="Home" component={Home} />
@@ -30,10 +34,34 @@ function App() {
     </Drawer.Navigator>
     </NavigationContainer>
 
+    
+    </>
     );
 }
 
 export default App;
+
+
+
+
+// import React from 'react';
+// import { createStackNavigator } from '@react-navigation/stack';
+
+// const Stack = createStackNavigator();
+
+// function MyStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Home" component={Home} />
+//       <Stack.Screen name="About" component={About} />
+//     </Stack.Navigator>
+//   );
+// }
+
+
+
+
+
 
 
 
--- Pizza/src/screens/Home.js
+++ Pizza/src/screens/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity, Button} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 // import Pizza1 from '../assets/img/Pizza_1'
 
@@ -22,8 +29,27 @@ const Home = () => {
         </LinearGradient>
       </View>
 
-      <View style={{borderWidth: 0,marginLeft: 30,marginRight: 30,backgroundColor: 'rgba(255, 255, 255, 0.6)',marginTop: -120,  borderRadius: 200,paddingTop: 20,paddingBottom: 20}}>
-        <View style={{borderWidth: 0,marginLeft: 30,marginRight: 30,backgroundColor: '#fcf8f6',  borderRadius: 200,paddingTop: 20,paddingBottom: 20}}>
+      <View
+        style={{
+          borderWidth: 0,
+          marginLeft: 30,
+          marginRight: 30,
+          backgroundColor: 'rgba(255, 255, 255, 0.6)',
+          marginTop: -120,
+          borderRadius: 200,
+          paddingTop: 20,
+          paddingBottom: 20,
+        }}>
+        <View
+          style={{
+            borderWidth: 0,
+            marginLeft: 30,
+            marginRight: 30,
+            backgroundColor: '#fcf8f6',
+            borderRadius: 200,
+            paddingTop: 20,
+            paddingBottom: 20,
+          }}>
           <View style={{alignItems: 'center', justifyContent: 'center'}}>
             <Image
               style={styles.MainPizza}
@@ -35,21 +61,47 @@ const Home = () => {
 
       <View style={styles.pizzaSizesBTNContainer}>
         <View style={{flex: 0.3}}></View>
-        <TouchableOpacity 
-        style={styles.pizzaSizesBTN}
-        >
-          <Text>
-            12*
-          </Text>
+        <TouchableOpacity style={styles.pizzaSizesBTN}>
+          <Text>12*</Text>
         </TouchableOpacity>
         <View style={{flex: 0.3}}></View>
       </View>
-        
 
       <View style={styles.pizzaSize}>
-      <Text style={styles.pizzaSizeHeading}>
-          Choose your size
-        </Text>
+        <Text style={styles.pizzaSizeHeading}>Choose your size</Text>
+        <View style={styles.sizeChoiceBTN}>
+          <TouchableOpacity style={styles.sizeChoiceSML}>
+            <Text>Small</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.sizeChoiceSML, styles.sizeChoiceBTNDesign]}>
+            <LinearGradient
+              colors={['#F5313F', '#FFA360']}
+              style={[styles.sizeChoiceBTNDesign, styles.mainBanner]}
+              start={{x: 0, y: 0.9}}
+              end={{x: 1, y: 0.2}}>
+              <Text style={{color: 'white'}}>Medium</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.sizeChoiceSML]}>
+            <Text>Large</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View>
+        <TouchableOpacity style={{marginTop: 20,}}>
+          <LinearGradient
+          colors={['#F5313F', '#FFA360']}
+          style={[styles.nextBTN]}
+          start={{x: 0, y: 0.9}}
+          end={{x: 1, y: 0.2}}
+          >
+          <Text style={{color: 'white', fontSize: 20,}}>
+            Next
+          </Text>
+          </LinearGradient>
+        </TouchableOpacity>
       </View>
     </>
   );
@@ -110,13 +162,13 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
   },
-  pizzaSizesBTNContainer:{
+  pizzaSizesBTNContainer: {
     // flex: 0.5,
     flexDirection: 'row',
     marginTop: 10,
     padding: 10,
   },
-  pizzaSizesBTN:{
+  pizzaSizesBTN: {
     alignItems: 'center',
     justifyContent: 'center',
     flex: 0.4,
@@ -125,26 +177,46 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 10,
     borderRadius: 20,
-    backgroundColor: '#dddddd'
+    backgroundColor: '#dddddd',
   },
-  pizzaSize:{
+  pizzaSize: {
     backgroundColor: '#fff',
     height: 120,
-    margin: 20,
+    marginHorizontal: 20,
     borderRadius: 30,
-    shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 2,
-},
-shadowOpacity: 0.25,
-shadowRadius: 3.84,
-elevation: 5,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 3.84,
+    shadowRadius: 0.84,
+    elevation: 1,
   },
-  pizzaSizeHeading:{
+  pizzaSizeHeading: {
     fontSize: 20,
     textAlign: 'center',
     paddingTop: 10,
+  },
+  sizeChoiceBTN: {
+    flexDirection: 'row',
+  },
+  sizeChoiceSML: {
+    flex: 0.33,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 3,
+  },
+  sizeChoiceBTNDesign: {
+    paddingVertical: 8,
+    paddingHorizontal: 18,
+    borderRadius: 20,
+  },
+  nextBTN:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
   }
 });
 
