6a955bd8aceb1e417c2706ef932db37b097a94c5
Kabeer Ali Alvi <kabeeralvi6568@gmail.com>
2023-01-03 20:28:14 +0500

    Home screen Done

---
+++ Pizza/src/components/tabNavigation.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+export default function Tabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
